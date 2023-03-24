2fa42033c3672a3f62166381229325fbac73f09e
Vladislavs <vladislavs.kazimirenko@gmail.com>
2023-03-24 20:09:06 +0200

    fixes

--- src/app.service.ts
+++ src/app.service.ts
@@ -24,39 +24,42 @@ export class AppService {
     //     `https://sfbay.craigslist.org/search/cta?auto_title_status=1&nearbyArea=102&nearbyArea=12&dd=ss&nearbyArea=187&nearbyArea=188&nearbyArea=189&nearbyArea=191&nearbyArea=208&nearbyArea=285&nearbyArea=346&nearbyArea=373&nearbyArea=43&nearbyArea=454&nearbyArea=456&nearbyArea=62&nearbyArea=63&nearbyArea=708&nearbyArea=709&nearbyArea=710&nearbyArea=92&nearbyArea=96&nearbyArea=97&purveyor=owner&searchNearby=2&sort=date&ddd=ddd#search=1~thumb~0~0`,
     //   ),
     // );
-    this.proxyRequest
-      .getProxyRequest(url + `&${randomQuery}=${randomQuery}`)
-      .then((body) => {
-        try {
-          this.htmlToJson.getJson(body).done((result) => {
-            var page = result;
 
-            let names =
-              result?.cars?.filter[0]?.name +
+    try {
+      let body = await firstValueFrom(
+        await this.proxyRequest.getProxyRequest(
+          url + `&${randomQuery}=${randomQuery}`,
+        ),
+      );
+      this.htmlToJson.getJson(body).done((result) => {
+        var page = result;
+
+        let names =
+          result?.cars?.filter[0]?.name +
+          result?.cars?.filter[1]?.name +
+          result?.cars?.filter[2]?.name
+            ? result?.cars?.filter[0]?.name +
               result?.cars?.filter[1]?.name +
               result?.cars?.filter[2]?.name
-                ? result?.cars?.filter[0]?.name +
-                  result?.cars?.filter[1]?.name +
-                  result?.cars?.filter[2]?.name
-                : '';
-            console.log(names);
-            const hash = crypto
-              .createHash('sha256')
-              .update(names.toString())
-              .digest('base64');
+            : '';
+        console.log(names);
+        const hash = crypto
+          .createHash('sha256')
+          .update(names.toString())
+          .digest('base64');
 
-            if (this.hash == hash && !isfirstTime) {
-              res.send({ data: 'No change', isNotNeeded: true });
-            } else {
-              this.hash = hash;
-              res.send(page);
-            }
-          });
-        } catch (error) {
-          console.log(error);
+        if (this.hash == hash && !isfirstTime) {
           res.send({ data: 'No change', isNotNeeded: true });
+        } else {
+          this.hash = hash;
+          res.send(page);
         }
       });
+    } catch (error) {
+      console.log(error);
+      res.send({ data: 'No change', isNotNeeded: true });
+    }
+
     // console.log(response.data);
 
     // console.log(this.hash + ' this is hash ass was', hash + ' this is not');
