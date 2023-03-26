a5ca791a0e77bb52fe957342e3a170394fc8eb97
Vladislavs <vladislavs.kazimirenko@gmail.com>
2023-03-26 21:49:11 +0300

    wip

--- src/hash/hash.service.ts
+++ src/hash/hash.service.ts
@@ -1,43 +1,36 @@
-
-
 import { Injectable } from '@nestjs/common';
-import  * as find from 'lodash.find';
+import * as find from 'lodash.find';
 import * as findIndex from 'lodash.findindex';
 var crypto = require('crypto');
 
 @Injectable()
 export class HashService {
+  hashList = [];
 
+  public hashExist(url, factor) {
+    console.log(this.hashList);
+    console.log(this.encode(factor));
+    const result = find(this.hashList, { url, hash: this.encode(factor) });
+    return result.url === url && result.hash === this.encode(factor);
+  }
 
-hashList = [];
+  public setUrlHash(url, factor) {
+    const index = findIndex(this.hashList, { url });
 
-    public hashExist(url, factor){ 
-
-        console.log(this.hashList);
-        console.log(this.encode(factor))
-       return !!find(this.hashList, { url, hash: this.encode(factor) });
-    }
-    
-    public setUrlHash(url, factor){
-
-      const index = findIndex(this.hashList, { url });
-
-        if(index > -1){
-          this.hashList.splice(index,1);
-        }
-        
-        this.hashList.push({
-            url,
-            hash: this.encode(factor)
-       });
-       
+    if (index > -1) {
+      this.hashList.splice(index, 1);
     }
 
-    private encode(factor){
-      return crypto
-        .createHash('sha256')
-        .update(factor.toString())
-        .digest('base64');
-    }
+    this.hashList.push({
+      url,
+      hash: this.encode(factor),
+    });
+  }
 
+  private encode(factor) {
+    return crypto
+      .createHash('sha256')
+      .update(factor.toString())
+      .digest('base64');
+  }
 }
